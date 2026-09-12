34412fd559455f625fe6da5faadd55b75322c4d1
Yakobo <chusiyakobo50@gmail.com>
2026-09-12 10:02:06 +0300

    feat(ds-icons): add Casuya bird logo component, bump to 0.1.1

--- packages/ds-icons/src/index.ts
+++ packages/ds-icons/src/index.ts
@@ -1,5 +1,7 @@
 export { Icon } from './Icon';
 export { icons } from './icons';
 export { IconSprite } from './IconSprite';
+export { Logo } from './Logo';
 
 export type { IconName, IconProps } from './Icon';
+export type { LogoProps } from './Logo';
